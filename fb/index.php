4713219007b0fffe6d102f3f8d23c6a99fbe52a1
shashank agarwal <shashank@shankee.com>
2014-09-20 06:38:10 -0400

    json

---
+++ fb/index.php
@@ -0,0 +1,39 @@
+<?php 
+require_once './vendor/autoload.php';
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+
+session_start();
+FacebookSession::setDefaultApplication('390445034339844', '503fdca1d2452d608562b675dbcefc05');
+
+#FacebookSession::setDefaultApplication('apid', 'appscret');
+$helper = new FacebookRedirectLoginHelper("http://108.61.131.41/the_game_choice/fb/index.php", $apiVersion = NULL);
+try {
+    $session = $helper->getSessionFromRedirect();
+} catch (FacebookRequestException $ex) {
+    // When Facebook returns an error
+} catch (\Exception $ex) {
+    // When validation fails or other local issues
+}
+if (isset($session)) {
+
+    $request = new FacebookRequest($session, 'GET', '/me');
+    $response = $request->execute();
+    $graphObject = $response->getGraphObject();
+    var_dump($graphObject);
+    $id = $graphObject->getProperty('id');
+
+	$request = new FacebookRequest($session, 'GET', '/me/interests');
+    $response = $request->execute();
+    $graphObject = $response->getGraphObject();
+    var_dump($graphObject);
+    $id = $graphObject->getProperty('id');
+  
+
+} else {
+    echo '<a href="' . $helper->getLoginUrl() . '">Login with Facebook</a>';
+}
+
+?>
---
+++ places/index.php
@@ -0,0 +1,88 @@
+<?php
+
+include('vendor/autoload.php');
+
+$final_answer = array();
+
+$type_post = $_GET["types"];
+#echo $type_post;
+get_places('33.7489954', '-84.3879824', '800',$type_post);
+
+//if (isset($_POST["types"])){
+	#echo $type_post;
+
+//	get_places('-33.86820', '151.1945860', '800',$type_post);
+//}
+
+$topics=array();
+
+function get_places($lat, $long, $radius, $types){
+$google_places = new joshtronic\GooglePlaces('AIzaSyARM9C4EtyNYH0mSk-iyUtAoEnj_8Yrixs');
+$google_places->location = array($lat, $long);
+$google_places->radius   = $radius;
+$google_places->output   ='json';
+$google_places->keyword    = $types;
+$results                 = $google_places->nearbySearch();
+
+$r = $results['results'];
+
+#var_dump($r);
+
+foreach ($r as $x){
+	$x = $x['types'];
+	foreach ($x as $y){
+		if ($topics[$y]!= ''){
+			$topics[$y] = $topics[$y] +1;
+		}
+		else
+			$topics[$y] = 1;
+	}
+	#var_dump($x['types']);
+}
+
+
+
+arsort($topics);
+
+$output = array_slice($topics, 0, 5);
+$i = 0;
+foreach ($output as $key => $value){
+
+	$final_answer['categories'][$i]=$key;
+
+	$i =$i +1;
+	#echo '<form action="'.$PHP_SELF.'" method = "GET"><input type="hidden" name="types" value="'.$key.'"><button type="submit">'.$key.' </button></form>';
+
+}
+	#var_dump($final_answer);
+?>
+
+<table>
+<?
+$i = 0;
+foreach ($r as $x){
+	#var_dump($x);
+	$final_answer['title'][$i]['name'] = $x['name'];
+	$final_answer['title'][$i]['icon'] = $x['icon'];
+	$final_answer['title'][$i]['lat'] = $x['geometry']['location']['lat'];
+	$final_answer['title'][$i]['lng'] = $x['geometry']['location']['lng'];
+	$final_answer['title'][$i]['vicinity'] = $x['vicinity'];
+	$i =$i +1;
+	#echo '<tr><td>'.$x['name'].'</td></tr>';
+	
+}
+	$answer = json_encode($final_answer);
+	echo $answer;
+#var_dump( $answer);
+?>
+</table>
+<?php
+
+
+}
+
+
+
+?>
+
+
