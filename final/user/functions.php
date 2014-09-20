524995cb4507915c64159c5c02c381eabe2f63ba
shashank agarwal <shashank@shankee.com>
2014-09-20 12:43:24 -0400

    finak

--- final/user/functions.php
+++ final/user/functions.php
@@ -1,7 +1,7 @@
 <?php
 require 'dbconfig.php';
 function checkuser($fuid,$funame,$ffname,$femail){
-    	$check = mysql_query("select * from Users where Fuid='$fbid'");
+    $check = mysql_query("select * from Users where Fuid='$fbid'");
 	$check = mysql_num_rows($check);
 	if (empty($check)) { // if new user . Insert a new record		
 	$query = "INSERT INTO Users (Fuid,Funame,Ffname,Femail) VALUES ('$fuid','$funame','$ffname','$femail')";
@@ -14,15 +14,19 @@ function checkuser($fuid,$funame,$ffname,$femail){
 	}
 }
 
-function addKey($key,$freq,$user,$femail){
-        $check = mysql_query("select * from Users where Fuid='$fbid'");
-        $check = mysql_num_rows($check);
-        if (empty($check)) { // if new user . Insert a new record
-        $query = "INSERT INTO Users (Fuid,Funame,Ffname,Femail) VALUES ('$fuid','$funame','$ffname','$femail')";
+function addKey($fb_id,$tag,$freq){
+        $check = mysql_query("select * from tags where fb_id='$fb_id' LIMIT 1");
+        
+        while ($row = mysql_fetch_assoc($result)) {
+        if ($row['1'] != $tag) { 
+        $query = "INSERT INTO tags (fb_id,keyword,frequency) VALUES ('$fb_id','$tag','1')";
         mysql_query($query);
         } else {   // If Returned user . update the user record
-        $query = "UPDATE Users SET Funame='$funame', Ffname='$ffname', Femail='$femail' where Fuid='$fuid'";
+        	$freq_new = $row['2'] +1;
+        $query = "UPDATE tags SET  frequency='$freq_new' where fb_id='$fb_id'";
         mysql_query($query);
         }
+    }
+    }
 
 ?>
--- final/user/index.php
+++ final/user/index.php
@@ -4,7 +4,7 @@ session_start();
 <!doctype html>
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
   <head>
-    <title>Login with Facebook</title>
+    <title>The Paradox Of Choice</title>
 <link href="http://www.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"> 
  </head>
   <body>
@@ -12,7 +12,7 @@ session_start();
 <div class="container">
 <div class="hero-unit">
   <h1>Hello <?php echo $_SESSION['USERNAME']; ?></h1>
-  <p>Welcome to "facebook login" tutorial</p>
+  <p>The Paradox Of Choice</p>
   </div>
 <div class="span4">
  <ul class="nav nav-list">
@@ -24,7 +24,9 @@ session_start();
 <li><?php echo $_SESSION['USERNAME']; ?></li>
 <li class="nav-header">Facebook fullname</li>
 
-
+<?
+addKey($_SESSION['FBID'],'test');
+?>
 <li><?php echo $_SESSION['FULLNAME']; ?></li>
 <div><a href="logout.php">Logout</a></div>
 </ul></div></div>
